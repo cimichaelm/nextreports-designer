1055a7fc8a8bb62ee5bff6df1ba9bc895e1f98f0
cimichaelm <michaelm@corp.citenet.net>
2021-07-23 10:30:49 -0400

    scripts

---
+++ scripts/fetch-data.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# fetch files
+#
+defaults()
+{
+    var="9.2"
+    filename="nextreports-designer-${ver}.zip"
+    url="http://www.asf.ro/next_reports_releases/${filename}"
+}
+
+fetch_data()
+{
+    
+    wget "${url}"
+    unzip "${filename}"
+
+}
+
+defaults
+
+fetch_data
+
+
